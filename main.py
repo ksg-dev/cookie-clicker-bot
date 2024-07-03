96ce9e2eea16068f18568096519aea9155643233
ksg-dev <ksg.dev.data@gmail.com>
2024-07-03 17:40:56 -0500

    fixed loop - simplify buying

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 # Use this class to use specific keys besides letters/numb
 from selenium.webdriver.common.keys import Keys
 
@@ -13,39 +14,32 @@ driver.get("https://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.ID, value="cookie")
 store = driver.find_element(By.ID, value="store")
 
-# Store Items
-# buy_cursor = driver.find_element(By.ID, value="buyCursor")
-# store_items = store.find_elements(By.CSS_SELECTOR, value="div b")
-# for item in store_items:
-#     print(item.text)
-
 
 def check_upgrades():
     store_items = store.find_elements(By.CSS_SELECTOR, value="div")
-    for item in store_items[7::-1]:
-        status = item.get_attribute("class")
-        item_title = item.find_element(By.CSS_SELECTOR, value="b").text
-        name, price = item_title.split(" - ")
-        # print(name)
-        # print(price)
-        if status != "grayed":
-            money = driver.find_element(By.ID, value="money").text
-            if int(money) >= int(price):
-                item.click()
+    for item in store_items[::-1]:
+        try:
+            item.get_attribute("onclick")
+            item.click()
+        except:
+            pass
+
+
+timeout = time.time() + 300
 
 
 def main():
-    click = 0
-    while click < 1000:
-        cookie.click()
-        if click >= 500:
-            check_upgrades()
-        click += 1
+    while time.time() < timeout:
+        first_loop = time.time() + 5
+        while time.time() < first_loop:
+            cookie.click()
+        check_upgrades()
+    cookies = driver.find_element(By.ID, value="cps").text
+    print(cookies)
 
 
 main()
-# print(buy_cursor.get_attribute("class"))
 
 
 # driver.quit()
-# cookie.click()
+
